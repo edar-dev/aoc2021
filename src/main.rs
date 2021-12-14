b3514d74924b0e3e443aa6ed3bd249d0d7b3f944
Edoardo Arlati <edoardo.arlati@triman.ch>
2021-12-14 14:59:44 +0100

    day one second part optimization

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,37 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
+
 fn main() -> io::Result<()> {
     // read input from file
     let file = File::open("src/input").expect("file not found");
     let reader = BufReader::new(file);
 
-    // create array of three integers to store the input
-    let mut input: [i32; 3] = [0; 3];
+    let mut input: Vec<i32> = Vec::new();
     // previous count of the three integers
-    let mut prev_count = 0;
-    let mut input_counter = 0;
     let mut increase_counter: i32 = 0;
-    let mut row = 0;
 
     for line in reader.lines() {
         let value: i32 = line.unwrap().parse().unwrap();
-        input[input_counter] = value;
-        input_counter += 1;
-        input_counter %= 3;
-        row += 1;
+        input.push(value);
 
-        if row >= 3 {
-            analyze_slice(&input, &mut prev_count, &mut increase_counter)
+        if input.len() < 4 {
+            continue;
         }
+
+        if analyze_slice(&input) {
+            increase_counter += 1;
+        }
+
+        input.remove(0);
     }
 
     println!("{}", increase_counter);
     Ok(())
 }
 
-fn analyze_slice(slice: &[i32; 3], prev_count: &mut i32, counter: &mut i32) {
-    let sum = slice.iter().sum::<i32>();
-    if *prev_count != 0 && sum > *prev_count {
-        *counter += 1;
-    }
-    *prev_count = sum;
+fn analyze_slice(slice: &[i32]) -> bool {
+    let prev_count = slice[0..3].iter().sum::<i32>();
+    let current_count = slice[1..4].iter().sum::<i32>();
+
+    current_count > prev_count
 }
