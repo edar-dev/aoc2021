32f6c532b1d6e66b4e42c92759ff308f5f95267f
Edoardo Arlati <edoardo.arlati@triman.ch>
2021-12-14 14:18:01 +0100

    day one second part

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,38 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 fn main() -> io::Result<()> {
-
     // read input from file
     let file = File::open("src/input").expect("file not found");
     let reader = BufReader::new(file);
 
+    // create array of three integers to store the input
+    let mut input: [i32; 3] = [0; 3];
+    // previous count of the three integers
+    let mut prev_count = 0;
+    let mut input_counter = 0;
+    let mut increase_counter: i32 = 0;
     let mut row = 0;
-    let mut prev_value = 0;
-    let mut counter =0;
+
     for line in reader.lines() {
-        if row == 0 {
-            prev_value = line.unwrap().parse::<i32>().unwrap();
-            println!("{} (N/A - no previous measurement)" , prev_value);
-        } else {
-            let current_value = line.unwrap().parse::<i32>().unwrap();
-            if current_value > prev_value {
-                println!("{} (increased)", current_value);
-                counter += 1;
-            } else {
-                println!("{} (decreased)" , current_value);
-            }
-            prev_value = current_value;
-        }
+        let value: i32 = line.unwrap().parse().unwrap();
+        input[input_counter] = value;
+        input_counter += 1;
+        input_counter %= 3;
         row += 1;
+
+        if row >= 3 {
+            analyze_slice(&input, &mut prev_count, &mut increase_counter)
+        }
     }
-    println!("{} FIANL VALUE", counter);
 
+    println!("{}", increase_counter);
     Ok(())
+}
 
+fn analyze_slice(slice: &[i32; 3], prev_count: &mut i32, counter: &mut i32) {
+    let sum = slice.iter().sum::<i32>();
+    if *prev_count != 0 && sum > *prev_count {
+        *counter += 1;
+    }
+    *prev_count = sum;
 }
